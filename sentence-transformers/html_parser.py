8aaeaa303ae5f5502eac60321f53a3c6a5dad3ab
DuyguA <duygu.altinok12@gmail.com>
2023-01-10 21:55:56 +0100

    fixed typo

--- sentence-transformers/html_parser.py
+++ sentence-transformers/html_parser.py
@@ -1,13 +1,15 @@
 from bs4 import BeautifulSoup
 
 
-html = open("data/kredi.html").read()
-soup = BeautifulSoup(html, 'lxml')
+def extract_text(html_page):
+  html = open(html_page).read()
+  soup = BeautifulSoup(html, 'lxml')
 
-lists = soup.find_all('body')
-for llist in lists:
+  lists = soup.find_all('body')
+  for llist in lists:
     qList = llist.find_all('ul')
     ansList = llist.find_all(['ol', "p"])
+  return qList, ansList
 
 
 def clean_tags(text):
@@ -28,8 +30,12 @@ def find_content(tag):
   return result
 
 
-for ques,ans in zip(qList, ansList):
-  ques = find_content(ques)
-  ans = find_content(ans)
-  print(ques, ans)
-  print("---------------")
+def extract_ques_and_answers(html_page):
+  qList, ansList = extract_text(html_page)
+  all_ques, all_answer = [], []
+  for ques,ans in zip(qList, ansList):
+    ques = find_content(ques)
+    ans = find_content(ans)
+    all_ques.append(ques)
+    all_answer.append(ans)
+  return all_ques, all_answer
